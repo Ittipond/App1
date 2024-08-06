219d9b9a1c035d370ed4f3ae68e714a895bb12c9
lumi <ittipond402@gmail.com>
2024-08-06 23:28:51 +0700

    add component

--- app1/src/App.js
+++ app1/src/App.js
@@ -1,10 +1,15 @@
 import React from 'react'
 import Calender from './class-components'
+import { Header, Content, Footer } from './func-componetent'
 
 export default function App() {
   return (
     <>
+    <Header/>
     <p><center><Calender/></center></p>
+    <Content/>
+    <Footer/>
+    
     </>
   )
 
