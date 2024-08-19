658fc694dd896ca0e5997693c4c4585fea2b552e
lumi <ittipond402@gmail.com>
2024-08-19 11:28:04 +0700

    Create state-func.js

--- app1/src/App.js
+++ app1/src/App.js
@@ -1,9 +1,9 @@
 import React from 'react'
 // import Button from './class-components'
 //import Calculator2 from './calculator'
-import RefsFunc from './refs-func.js'
-
+import RefsFunc from './refs-func'
+import MessageBox from './state-func'
 
 export default function App() {
-  return <RefsFunc/>
+  return <MessageBox/>
 }
