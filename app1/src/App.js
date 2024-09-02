0b08cd9c583190d8b815bc59166a1e7d85932805
lumi <ittipond402@gmail.com>
2024-09-02 21:58:44 +0700

    create context2

--- app1/src/App.js
+++ app1/src/App.js
@@ -1,9 +1,18 @@
-import React from 'react'
 // import Button from './class-components'
 //import Calculator2 from './calculator'
-import RefsFunc from './refs-func'
-import MessageBox from './state-func'
+// import RefsFunc from './refs-func'
+// import MessageBox from './state-func'
+import { userContext } from './context'
+import React from 'react'
+import Header2 from './context-header2'
+import Context2 from './context-content2'
 
 export default function App() {
-  return <MessageBox/>
+  let [user,setUser] = React.useState('')
+  return(
+    <userContext.Provider value={[user,setUser]}>
+      <Header2/>
+      <Context2/>
+    </userContext.Provider>
+  )
 }
---
+++ app1/src/context-content2.js
@@ -0,0 +1,7 @@
+import React from "react";
+import { useContext } from "./context";
+
+export default function Context2(){
+    let [user,setUser] = React.useState('')
+    
+}
